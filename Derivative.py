7227f7bf510c6ecfcac8c22521c45911ab089b3e
John Loverich <john.loverich@gmail.com>
2020-01-14 20:55:41 -0800

    Seems to be working.

--- Derivative.py
+++ Derivative.py
@@ -5,8 +5,6 @@ Takes in an image as a 2 (or 3) dimensional numpy vector of the form [x, y, colo
 wave limited (Total Variation Diminishing) derivatives.
 '''
 def computeDerivatives(image) :
-  
-    print('inside image', image.shape)
 
     xSize = image.shape[0]
     ySize = image.shape[1]
@@ -15,22 +13,21 @@ def computeDerivatives(image) :
     dxc = np.arange(1, xSize-1)
     dxl = np.arange(0, xSize-2)
     dxr = np.arange(2, xSize)
-    y = np.arange(1, ySize-1)
 
-    dL = image[dxl,:]-image[dxc,:]
-    dR = image[dxc,:]-image[dxr,:]
+    dL = image[dxc,:]-image[dxl,:]
+    dR = image[dxr,:]-image[dxc,:]
     dc = 0.5*(dL+dR)
+
     dI = minmod(dL, dR, dc)
     dI = dI[:,1:(ySize-1)]
 
     #compute dy
     dyc = np.arange(1, ySize-1)
-    dyt = np.arange(0, ySize-2)
-    dyb = np.arange(2, ySize)
-    x = np.arange(1, xSize-1)
+    dyl = np.arange(0, ySize-2)
+    dyr = np.arange(2, ySize)
 
-    dL = image[:,dyt]-image[:,dyc]
-    dR = image[:,dyc]-image[:,dyb]
+    dL = image[:,dyc]-image[:,dyl]
+    dR = image[:,dyr]-image[:,dyc]
     dc = 0.5*(dL+dR)
 
     dJ = minmod(dL, dR, dc)
@@ -42,19 +39,14 @@ def streamVectors(image) :
     dX, dY = computeDerivatives(image)
     return dY, -dX
 
-def minmod(a, b, c) :
+def gradientMagnitude(image) :
+    dX, dY = computeDerivatives(image)
+    return np.sqrt(dX*dX+dY*dY)
 
-  shape = a.shape
-  a=a.flatten()
-  b=b.flatten()
-  c=c.flatten()
+def minmod(a, b, c) :
 
   xl = np.where((np.absolute(a) < np.absolute(b)) & (np.absolute(a) < np.absolute(c)), a, 0)
   xr = np.where((np.absolute(b) < np.absolute(a)) & (np.absolute(b) < np.absolute(c)), b, 0)
   xc = np.where((np.absolute(c) <= np.absolute(a)) & (np.absolute(c) <= np.absolute(b)), c, 0)
   
-  xl=xl.reshape(shape)
-  xr=xr.reshape(shape)
-  xc=xc.reshape(shape)
-
   return xl+xr+xc
